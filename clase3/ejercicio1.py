58da2d0d282fc068300b3dfb9aa8ce7e78ac71f9
fjsalgado <fjsalgado@minsait.com>
2021-11-17 13:36:29 +0100

    Correcciones

--- clase3/ejercicio1.py
+++ clase3/ejercicio1.py
@@ -1,4 +1,9 @@
 # Ejercicio 1: Crear un programa que le pregunte al usuario su nombre y edad y le muestre el año en que cumplirá los 100 años. 
+import datetime
+
+anhoActual = date.today().year
+
+nombre = input("Indique su nombre: ")
 edad = input("Indique su edad: ")
-calculo = 2021 + (100 - int(edad))
-print("Cumplirás 100 años en el ", calculo)
+calculo = int(anhoActual) + (100 - int(edad))
+print(nombre + ", cumplirás 100 años en el", calculo)
