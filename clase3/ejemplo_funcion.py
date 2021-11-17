2ea84a413c59bce565fc64bc5b287c4502f8ac3d
fjsalgado <fjsalgado@minsait.com>
2021-11-17 11:54:28 +0100

    ejemplo

---
+++ clase3/ejemplo_funcion.py
@@ -0,0 +1,8 @@
+# Ejemplo función
+# Pregunta el nombre y saluda al usuario
+def preguntar_nombre():
+    nombre = input("Cuál es tu nombre? ")
+    print("Hola", nombre)
+    return # opcional si no hay nada que retornar
+
+preguntar_nombre()
